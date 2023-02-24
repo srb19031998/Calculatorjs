8bb07d8ab45580f648bb4f7af01ccad8f24489eb
srb19031998 <122660917+srb19031998@users.noreply.github.com>
2023-02-24 18:45:13 +0530

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,12 @@
+function AC(){
+  document.getElementById("res").value="";
+}
+
+function show(input){
+  document.getElementById("res").value+=input;
+}
+
+function cal(){
+  var output =eval(document.getElementById("res").value)
+  document.getElementById("res").value=output;
+}
